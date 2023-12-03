0a17934079dd4a107b0acfa3af385e083ec7d121
TatianaDubos <tdubos@axor.com>
2023-12-03 14:40:51 -0500

    new class

---
+++ AxorP1/Class/PanelObjects.cs
@@ -0,0 +1,13 @@
+﻿namespace AxorP1.Class
+{
+	public class PanelObject
+	{
+		public string Id { get; set; }
+		public string Title { get; set; }
+		public int Column { get; set; }
+		public int Row { get; set; }
+		public int SizeX { get; set; }
+		public int SizeY { get; set; }
+		public System.Type ComponentType { get; set; }
+	}
+}
