16f102a9a8a12c233acbf63d68b9d6bef785b5ab
TatianaDubos <tdubos@axor.com>
2023-12-03 13:30:57 -0500

    Ajoutez des fichiers projet.

---
+++ AxorP1/Program.cs
@@ -0,0 +1,32 @@
+﻿using AxorP1;
+using AxorP1.Components;
+using Syncfusion.Blazor;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddSyncfusionBlazor();
+
+var app = builder.Build();
+//Register Syncfusion license https://help.syncfusion.com/common/essential-studio/licensing/how-to-generate
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MjIzNzgwMEAzMjMxMmUzMDJlMzBtU2IzQjBUaUI3NFpqdzJUMWhqYVRqd0xmQUxXMnVCSHg2MmJRTU9IMGRNPQ==");
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
