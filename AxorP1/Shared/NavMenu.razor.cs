a310d21ff4bbee43a9ea18ea6cc0a469d1186c6a
TatianaDubos <tdubos@axor.com>
2023-12-15 13:50:18 -0500

    Correcting name error

--- AxorP1/Shared/NavMenu.razor.cs
+++ AxorP1/Shared/NavMenu.razor.cs
@@ -7,13 +7,12 @@ namespace AxorP1.Shared
     public class NavMenuBase : MainComponent<NavMenu>
     {
         protected SfSidebar SidebarRef;
-
         // Specify the value of Sidebar component state (open/close).
         protected bool SidebarToggle = false;
         protected string ToggleClass = "close";
 
         // Event handler for Clicked event. It's used to open/close the Sidebar component. 
-        public void ToggleSidebar(Microsoft.AspNetCore.Components.Web.MouseEventArgs args)
+        private void ToggleSidebar(Microsoft.AspNetCore.Components.Web.MouseEventArgs args)
         {
             SidebarToggle = !SidebarToggle;
             ToggleClass = SidebarToggle ? "open" : "close";
